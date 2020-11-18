d30e95a6a881ddfab4c79363dc554a3e367a3f9c
totherain155 <totherain155@gmail.com>
2020-11-18 16:58:45 +0900

    rendering practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react";
+
+function Food({name, picture}){
+   return (
+      <div>
+      <h2>I wana have {name}</h2>
+      <img src = {picture} />
+   </div>
+   )
+}
 
 
-function App() {
-  return <div><h1>Hello!!</h1></div>;
-  
-} /*모든 react application을 div 사이에 넣는다 */
+const foodILike = [{
+    name : "noodle",
+    image : 
+    "https://thewoksoflife.com/wp-content/uploads/2020/04/homemade-chinese-egg-noodles-19.jpg"
+    
+
+},
+{
+   name : "Fried Chicken",
+   image : 
+    "https://www.melissassouthernstylekitchen.com/wp-content/uploads/2019/02/SouthernFriedChicken002.jpg"
+},{
+   name : "beef",
+   image : 
+   "https://challengedairy.com/sites/default/files/recipe/images/recipe_roast_beef_with_tangy_mustard_sauce_2280.jpg"
+}, {
+   name : "kebap",
+   image : 
+   "https://c8.alamy.com/comp/W4TM45/dner-kebap-on-a-plate-W4TM45.jpg"
+}
+ ]
+
+function App(){
+   
+   return (
+   <div>
+        <h1>Welcome !!</h1>
+        {foodILike.map(menu => <Food name={menu.name} picture ={menu.image}/> )}
+        
+
+   </div>
+   
+   )
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('potato')
+  /*react applicaion은 한 번에 하나의 component만 redering할 수 
+  있다. */
+  <App />,
+ /*<App />와 같은 것을 component라 부른다. react는 component와 
+  함께 동작한다. 모든 것은 component이다. 
+  component가 data를 보여주게 할 것이다.
+  <App />부분이 component를 사용하고자 할 때, component의 형태이다.
+  javascript와 HTML 사이의 이러한 조합을 jsx라고 부른다.
+  
+  jsx는 javascript안의 HTML이다. Component를 만들고 어떻게 사용하는지에 
+  대한 것이다.*/
+  document.getElementById('potato') 
 );
 
