1a9ac18040437ace6bf70ccfab5ea4f936a6fc0f
totherain155 <totherain155@gmail.com>
2020-11-21 19:47:21 +0900

    review property

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 import React from "react";
 
+function Music({ name, artist, rating }) {
+   return (
+      <div>
+         <h2> I like {name} </h2>
+         <img src={artist} alt={name} />
+         <h3> {rating} / 5.0</h3>
+      </div>
 
-class App extends React.Component {
-   state = {
-      count: 0
-   }
 
-   add = () => {
-      this.setState(current => ({ count: current.count + 1 }));
-   }
-   minus = () => {
-      this.setState(current => ({ count: current.count - 1 }));
-   }
+   )
+}
 
-   render() {
-      return (
-         <div>
-            <h1>The number is : {this.state.count}</h1>
-            <button onClick={this.add}>add</button>
-            <button onClick={this.minus}>minus</button>
-         </div>
-      )
-   }
+const MusicILike = [{
+   id: 1,
+   name: "Stranger in Moscow",
+   artist: "https://i.ytimg.com/vi/t6Ke4uD7CJc/hqdefault.jpg",
+   rating: 4.5
+}, {
+   id: 2,
+   name: "Faith",
+   artist: "https://ychef.files.bbci.co.uk/976x549/p08qvzz5.jpg",
+   rating: 4
+}, {
+   id: 3,
+   name: "Rocket Man",
+   artist: "https://www.udiscovermusic.com/wp-content/uploads/2020/03/Elton-John-Made-In-England-album-cover-820-820x600.jpg",
+   rating: 4.7
+}]
+
+
+
+function App() {
+   return (
+      <div>
+         {MusicILike.map(groove =>
+            <Music key={groove.id}
+               name={groove.name}
+               artist={groove.artist}
+               rating={groove.rating} />
+         )
+            //funciton, return 생략
+
+         }
+      </div>
+   )
 }
 
 
 
-
-
-
 export default App;
+
+
+
