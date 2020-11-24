89896c49360daca4bdd3a9e01240a2313e9b2a74
totherain155 <totherain155@gmail.com>
2020-11-24 23:29:36 +0900

    review state again

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React from "react";
 
-function Music({ name, artist, rating }) {
-   return (
-      <div>
-         <h2> I like {name} </h2>
-         <img src={artist} alt={name} />
-         <h3> {rating} / 5.0</h3>
-      </div>
 
+class App extends React.Component {
 
-   )
-}
+   static = {
+      count: 0
+   }
+   add = () => {
+      console.log("plus")
+   }
+   minus = () => {
+      console.log("minus")
+   }
 
-const MusicILike = [{
-   id: 1,
-   name: "Stranger in Moscow",
-   artist: "https://i.ytimg.com/vi/t6Ke4uD7CJc/hqdefault.jpg",
-   rating: 4.5
-}, {
-   id: 2,
-   name: "Faith",
-   artist: "https://ychef.files.bbci.co.uk/976x549/p08qvzz5.jpg",
-   rating: 4
-}, {
-   id: 3,
-   name: "Rocket Man",
-   artist: "https://www.udiscovermusic.com/wp-content/uploads/2020/03/Elton-John-Made-In-England-album-cover-820-820x600.jpg",
-   rating: 4.7
-}]
-
-
-
-function App() {
-   return (
-      <div>
-         {MusicILike.map(groove =>
-            <Music key={groove.id}
-               name={groove.name}
-               artist={groove.artist}
-               rating={groove.rating} />
-         )
-            //funciton, return 생략
-
-         }
-      </div>
-   )
+   render() {
+      return (
+         <div>
+            <h2>my number is : {this.static.count}</h2>
+            <button onClick={this.add}>add</button>
+            <button onClick={this.minus}>minus</button>
+         </div>
+      )
+   }
 }
 
 
 
 export default App;
-
-
-
