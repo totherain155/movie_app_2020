4fa5bcf0f8ef60dde8b56f359cbcc1a04484296a
totherain155 <totherain155@gmail.com>
2020-12-01 14:09:49 +0900

    reviw react_state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React from "react";
 
 
+
+
 class App extends React.Component {
 
-   static = {
+   state = {
       count: 0
    }
+
    add = () => {
-      console.log("plus")
+      console.log("add")
    }
+
    minus = () => {
       console.log("minus")
    }
 
+
+
    render() {
       return (
          <div>
-            <h2>my number is : {this.static.count}</h2>
+            <h2>the number is : {this.state.count}</h2>
             <button onClick={this.add}>add</button>
             <button onClick={this.minus}>minus</button>
          </div>
       )
    }
 }
+
+
+
+
+
+
+
 
 
 
