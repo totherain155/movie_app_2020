4c3bda1f05ae678fc65d9231232b116197c5a8c4
totherain155 <totherain155@gmail.com>
2020-12-09 17:00:55 +0900

    Building the Router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-
 import React from "react";
-import axios from "axios";
-import Movie from "./movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Potato from "./routes/About";
+import Home from "./routes/Home";
 
 
-class App extends React.Component {
-   state = {
-      isLoading: true,
-      movies: []
-   }
-
-
-   getMovies = async () => {
-      const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
-      //console.log(movies)
-      this.setState({ movies: movies, isLoading: false })
-   }
-
-
-   componentDidMount() {
-      this.getMovies()
-   }
-
-
-   render() {
-      const { isLoading, movies } = this.state
-      return (
-         <section className="container">
-            {isLoading ? <div className="loader">
-               <span className="loader_text">please wait...</span>
-            </div> : <div className="movies">
-                  {movies.map(art =>
-                     <Movie key={art.id}
-                        id={art.id}
-                        title={art.title}
-                        year={art.year}
-                        summary={art.summary}
-                        poster={art.medium_cover_image}
-                        genres={art.genres}
-
-                     />
-                  )}
-               </div>
-            }
-         </section>
-      )
-   }
-
+function App() {
+   return <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={Potato} />
+   </HashRouter>
 }
-
 
 export default App;
